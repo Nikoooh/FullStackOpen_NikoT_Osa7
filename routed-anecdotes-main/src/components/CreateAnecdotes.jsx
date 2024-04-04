0398f6077a63e613f057a61b56527a8ebd8c3c7c
Nikoooh <nikotoivanen1999@gmail.com>
2024-04-04 17:48:35 +0300

    7.4 - 7.8

--- routed-anecdotes-main/src/components/CreateAnecdotes.jsx
+++ routed-anecdotes-main/src/components/CreateAnecdotes.jsx
@@ -1,54 +1,60 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = ({ setAnecdotes, anecdotes, setNotification }) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
-    
-    const navigate = useNavigate()
+  const {reset: resetContent, ...content} = useField('text')
+  const {reset: resetAuthor, ...author} = useField('text')
+  const {reset: resetInfo, ...info} = useField('text')
+  
+  const navigate = useNavigate()
+  const addNew = (anecdote) => {
+      anecdote.id = Math.round(Math.random() * 10000)
+      setAnecdotes(anecdotes.concat(anecdote))
+      setNotification(`new anecdote ${anecdote.content} created`)
+      setTimeout(() => {
+        setNotification('')
+      }, 5000)
+      navigate('/')
+  }
 
-    const addNew = (anecdote) => {
-        anecdote.id = Math.round(Math.random() * 10000)
-        setAnecdotes(anecdotes.concat(anecdote))
-        setNotification(`new anecdote ${anecdote.content} created`)
-        setTimeout(() => {
-          setNotification('')
-        }, 5000)
-        navigate('/')
-    }
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      addNew({
-        content,
-        author,
-        info,
-        votes: 0
-      })
-    }
-  
-    return (
-      <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-          </div>
-          <div>
-            author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-          </div>
-          <button>create</button>
-        </form>
-      </div>
-    )
-  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+  }
+
+  const resetFields = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button type='submit'>create</button>
+        <button type='button' onClick={resetFields}>reset</button>
+      </form>  
+    </div>
+  )
 }
 
 export default CreateNew
